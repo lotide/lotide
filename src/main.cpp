4413ef7dda2de11d059d786335a6eb1364f09ccd
Ian Park <duckonomy@gmail.com>
2019-12-02 15:05:11 -0500

    fix include

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 #include "LoTideCli.hpp"
-#include "LoTideServer.hpp"
+/* #include "LoTideServer.hpp" */
 
 using namespace lotide;
 
 int main(int argc, char *argv[])
 {
-    /* LoTideCli cli; */
-    /* cli.launch(); */
+    LoTideCli cli;
+    cli.launch();
 
-    LoTideServer ser;
-    ser.launch();
+    /* LoTideServer ser; */
+    /* ser.launch(); */
 
     return 0;
 }
